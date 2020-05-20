1e83b378ca6dde9064983752ba251c11bcf3f2da
Usama Awan <underusama@gmail.com>
2020-05-20 20:05:06 +0500

    redirect for landing page

--- client/src/components/layout/landing/Landing.js
+++ client/src/components/layout/landing/Landing.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Hero from './Hero';
+import { Redirect } from 'react-router-dom';
 import Features from './Features';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Landing = () => (
-  <React.Fragment>
-    <Hero />
-    <Features />
-  </React.Fragment>
-);
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/builds' />;
+  }
+  return (
+    <React.Fragment>
+      <Hero />
+      <Features />
+    </React.Fragment>
+  );
+};
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
